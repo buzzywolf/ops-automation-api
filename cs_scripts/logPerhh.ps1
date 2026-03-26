1bd4ed7a3aa382a4bd4ef77c62cbafde646f693b
buzzy wolf <eyal123st@gmail.com>
2026-03-26 19:04:14 +0200

    added flask and changed the code a little bit

--- cs_scripts/logPerhh.ps1
+++ cs_scripts/logPerhh.ps1
@@ -1,31 +1,32 @@
 
-$list = Get-WinEvent  -FilterHashtable @{LogName = 'Security';Id =4625}
+$list = Get-WinEvent  -FilterHashtable @{LogName = 'Security';Id =4625} -ErrorAction SilentlyContinue
 
 $startTime = (Get-Date).AddMinutes(-30)
 $arr=@()
 ForEach ($log in $list){
-if($log.StartTime -ge $startTime)
+if($log.TimeCreated -ge $startTime)
 {
 $arr+=$log
 }
 }
 $LISTOFip=@()
-Write-Host "the next ip adresss failed to enter your computer"
+#Write-Host "the next ip adresss failed to enter your computer"
+
 ForEach ($log in $arr){
 $xmlEV=[xml]$log.ToXml()
 $ip = ($xmlEV.Event.EventData.Data | where-Object {$_.Name -eq "IpAddress"}).'#text'
 $LISTOFip+=$ip
-Write-Host $ip
+#Write-Host $ip
 }
 
 
-
+$attackARR=@()
 foreach ($LIS in $LISTOFip)
 {
 $counter=0
 foreach ($LI in $LISTOFip)
 {
-$attackARR=@()
+
 if($LI -eq $LIS)
 {
 $counter+=1
@@ -33,11 +34,11 @@ $counter+=1
 
 }
 $counter = $counter
-Write-Host "the ip address:" $LIS "attacked:" $counter "times in the last 30 minutes"
+# Write-Host "the ip address:" $LIS "attacked:" $counter "times in the last 30 minutes"
 if( $counter -gt 3)
 {
    $attackARR+=$LIS
 }
 }
-$attackARR | ConvertTo-Json
-
+# Convert to JSON, and ensure we only output unique IPs, forced as an array
+@($attackARR | Select-Object -Unique) | ConvertTo-Json
--- myapp.py
+++ myapp.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
 import subprocess
 import json
-
+#name ==name of the file that runs at the moment
 app = Flask(__name__)#משתנה זה יחזיר לנו את השרת שהרמנו
 # מגדירים לאיזו כתובת הלקוח נכנס (במקרה הזה - דף הבית)
-@app.route('/')
-def display:
+@app.route('/')#'/' is the URL that when we are at him, the bellow function will work.
+# This means the home page
+def display():
     sc_path=r"C:\Users\User\AutoLog\cs_scripts\logPerhh.ps1"
     command = ["powershell.exe", "-ExecutionPolicy", "Bypass", "-File", sc_path]
     result = subprocess.run(command, capture_output=True, text=True)
